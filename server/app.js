617a09db1e060411f617c92c63d930ab8de1a38e
oGsLP <1145234011@qq.com>
2020-05-01 16:01:56 +0800

    :heavy_minus_sign: remove cookie-parser dep, config express-session

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const helmet = require("helmet");
 const session = require("express-session");
@@ -37,11 +37,20 @@ app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "dist")));
 
 app.use(
   session({
+    name: "sid",
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24
+    },
+    secret: "qifei",
+    resave: false,
+    saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: connection })
   })
 );
