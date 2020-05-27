fe92c2278b2b45a5571159598c61fe6f51a414aa
oGsLP <1145234011@qq.com>
2020-05-27 13:52:40 +0800

    :construction: finish teacherProfile

--- server/routes/teacher/info.js
+++ server/routes/teacher/info.js
@@ -8,14 +8,20 @@
 const router = require("express").Router();
 const userService = require("./../../services/serviceFactory").UserService();
 const Message = require("./../../utils/message");
+const firstToUpperCase = require("./../../utils/util").firstToUpperCase;
 
 router.get("/", async function(req, res, next) {
   let tid = res.locals["teacherID"];
   // logic process
   // info是一个[]，第一项是id，第二项是userSchema实体类，get获得数据
   let info = await userService.getUserInformation(tid);
-  // Todo process
-  res.json(new Message(true, info, ""));
+  let data = {
+    id: info[0],
+    name: info[1]["userName"],
+    type: firstToUpperCase(info[1]["userType"]),
+    org: info[1]["orgID"]
+  };
+  res.json(new Message(true, data, ""));
 });
 
 module.exports = router;
--- src/views/teacher/profile/index.vue
+++ src/views/teacher/profile/index.vue
@@ -1,16 +1,54 @@
 <template>
-  <div>{{ info }}</div></template
->
+  <div class="teacher-profile">
+    <h1>Profile</h1>
+    <el-divider />
+    <el-row :gutter="25">
+      <el-col span="9">
+        <el-avatar :size="180">
+          <img src="./../../../assets/laoge.png" alt="" />
+        </el-avatar>
+        <p>Avatar</p>
+      </el-col>
+      <!--      <el-col span="3"> dsa</el-col>-->
+      <el-col :offset="1" span="12">
+        <el-form label-position="top" label-width="80px">
+          <el-form-item label="User ID">
+            <el-input v-model="info.id" readonly></el-input>
+          </el-form-item>
+          <el-form-item label="Username">
+            <el-input v-model="info.name" readonly></el-input>
+          </el-form-item>
+          <el-form-item label="User Type">
+            <el-radio-group :value="info.type">
+              <el-radio-button
+                v-for="type in user_types"
+                :key="type"
+                :label="type"
+                :disabled="info.type !== type"
+              ></el-radio-button>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="Orgnization">
+            <el-input v-model="info.org" readonly></el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
 
 <script>
 export default {
   name: "TeacherProfile",
   data() {
     return {
+      user_types: ["Member", "Teacher"],
       info: {}
     };
   },
-  created() {},
+  created() {
+    this.get_info();
+  },
   mounted() {},
   computed: {
     teacherID: () => sessionStorage.getItem("id")
@@ -20,11 +58,27 @@ export default {
       this.$axios
         .get(`/api/teacher/${encodeURIComponent(this.teacherID)}/info`)
         .then(res => {
-          this.info = res.data;
+          this.info = res.data.data;
         });
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style lang="stylus" scoped>
+.teacher-profile
+  width 100%
+  height 100%
+  &>h1
+    margin 0
+    width 100%
+    text-align left
+    left 0
+    font-size 32px
+    line-height 32px
+    font-weight 400
+  &>.el-divider
+    margin 16px 0 48px
+.el-form
+  text-align left
+</style>
