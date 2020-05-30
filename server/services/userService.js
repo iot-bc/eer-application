4360d2a30edf74ccf2bbea5e03043c0a62fe9b1d
xiongyiheng <763285416@qq.com>
2020-05-30 14:19:56 +0800

    revise user service

--- server/services/userService.js
+++ server/services/userService.js
@@ -129,7 +129,7 @@ function UserService() {
       }
     );
     const url =
-      "http://120.26.172.10:48080/api/v1/event/device/" + _deviceID + "/1";
+      "http://120.26.172.10:48080/api/v1/event/device/" + _deviceID + "/8";
 
     return await axios.get(url).then(res => {
       let list = [];
@@ -319,7 +319,7 @@ function UserService() {
       }
     );
     const url =
-      "http://120.26.172.10:48080/api/v1/event/device/" + _deviceID + "/1";
+      "http://120.26.172.10:48080/api/v1/event/device/" + _deviceID + "/8";
 
     return await axios.get(url).then(res => {
       let list = [];
